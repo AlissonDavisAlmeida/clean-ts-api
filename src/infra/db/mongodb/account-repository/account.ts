0838800d3ec151faa9088c671ba9a036311a1516
alisson <alissoneatena1@gmail.com>
2023-09-20 11:33:44 -0300

    feat: ensure AccountMongoRepository returns an account on success

---
+++ src/infra/db/mongodb/account-repository/account.ts
@@ -0,0 +1,23 @@
+import { type AddAccountRepository } from '../../../../data/protocols/addAccountRepository';
+import { type AccountModel } from '../../../../domain/models/AccountModel';
+import { type AddAccountModel } from '../../../../domain/useCases/addAccount';
+import { mongoHelper } from '../helpers/mongo-helper';
+
+export class AccountMongoRepository implements AddAccountRepository {
+  async add (accountData: AddAccountModel): Promise<AccountModel> {
+    const accountCollection = mongoHelper.getCollection('accounts');
+    const result = await accountCollection.insertOne(accountData);
+
+    if (!result.acknowledged) {
+      throw new Error('Error on insert new account');
+    }
+
+    const account = await accountCollection.findOne({ _id: result.insertedId });
+
+    if (!account) {
+      throw new Error('Error on find inserted account');
+    }
+
+    return mongoHelper.map(account);
+  }
+}
--- src/infra/db/mongodb/helpers/mongo-helper.ts
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -8,5 +8,14 @@ export const mongoHelper = {
   },
   async disconnect (): Promise<void> {
     await this.client.close();
+  },
+
+  getCollection (name: string) {
+    return this.client.db().collection(name);
+  },
+
+  map (collection: any): any {
+    const { _id, ...collectionWithoutId } = collection;
+    return Object.assign({}, collectionWithoutId, { id: _id });
   }
 };

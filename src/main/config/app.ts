dd82ccfec224fa2aa0d71a2c8ffc461603b0c90d
alisson <alissoneatena1@gmail.com>
2023-10-17 14:08:31 -0300

    feat: ensure Signup route returns an account on success

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -1,6 +1,8 @@
 import express from 'express';
 import { middlewaresConfig } from './middlewares';
+import { routesConfig } from './routes';
 
 export const app = express();
 
 middlewaresConfig(app);
+routesConfig(app);

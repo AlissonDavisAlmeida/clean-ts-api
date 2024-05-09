377d928e90315cc57b96d77470319db0ac57b2a7
alisson <alissoneatena1@gmail.com>
2024-05-09 14:51:08 -0300

    feat: ensure JWTAdapter returns a token when sign succeds

--- src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
+++ src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
@@ -1,22 +1,37 @@
 import jwt from 'jsonwebtoken';
 import { JWTAdapter } from './jwt-adapter';
 
-const makeSut = (): JWTAdapter => {
-  return new JWTAdapter();
+jest.mock('jsonwebtoken', () => ({
+  async sign (): Promise<string> {
+    return 'any_token';
+  }
+}));
+
+interface SutTypes {
+  sut: JWTAdapter
+  secret: string
+}
+
+const makeSut = (): SutTypes => {
+  const secret = 'secret';
+  return {
+    sut: new JWTAdapter(secret),
+    secret
+  };
 };
 describe('Jwt Adapter', () => {
   test('should calls sign method with correct values', async () => {
-    const sut = makeSut();
+    const { sut, secret } = makeSut();
     const id = 'any_id';
     const signSpy = jest.spyOn(jwt, 'sign');
 
     await sut.generate(id);
 
-    expect(signSpy).toHaveBeenCalledWith({ id }, 'secret');
+    expect(signSpy).toHaveBeenCalledWith({ id }, secret);
   });
 
   test('should throw if sign throws', async () => {
-    const sut = makeSut();
+    const { sut } = makeSut();
     jest.spyOn(jwt, 'sign').mockImplementationOnce(() => {
       throw new Error();
     });
@@ -24,5 +39,12 @@ describe('Jwt Adapter', () => {
     const promise = sut.generate('any_id');
 
     await expect(promise).rejects.toThrow();
+  });
+
+  test('should return a token on sign success', async () => {
+    const { sut } = makeSut();
+    const accessToken = await sut.generate('any_id');
+
+    expect(accessToken).toBe('any_token');
   });
 });
--- src/infra/criptography/jwt-adapter/jwt-adapter.ts
+++ src/infra/criptography/jwt-adapter/jwt-adapter.ts
@@ -2,8 +2,12 @@ import { type Encrypter } from '@/@data/protocols/criptography';
 import jwt from 'jsonwebtoken';
 
 export class JWTAdapter implements Encrypter {
+  constructor (
+    private readonly secret: string
+  ) {}
+
   async generate (id: string): Promise<string> {
-    const token = jwt.sign({ id }, 'secret');
+    const token = jwt.sign({ id }, this.secret);
 
     return token;
   }

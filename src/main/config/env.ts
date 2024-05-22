1875ddedf886919b029acf36b8e6838f30507401
alisson <alissoneatena1@gmail.com>
2024-05-22 09:33:02 -0300

    refactor: move url mongo to .env file

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,5 +1,5 @@
 export const config = {
-  mongoUrl: process.env.MONGO_URL ?? 'mongodb+srv://Alisson:hwDtOECNmMcd2Cap@cluster0.pgaza.mongodb.net/clean-node-api?retryWrites=true&w=majority',
+  mongoUrl: process.env.MONGO_URL,
   port: process.env.PORT ?? 3000,
   jwtSecret: process.env.JWT_SECRET ?? 'tj670==5H'
 };

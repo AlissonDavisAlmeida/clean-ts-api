e67db015be54c22eaca43c794a6ceb15deb2ddc5
alisson <alissoneatena1@gmail.com>
2024-07-02 15:33:30 -0300

    feat: ensure LoadAccountByTokenUseCase returns null if LoadAccountByTokenRepository return null

--- src/@data/usecases/account/load-account-by-token/load-account-by-token.ts
+++ src/@data/usecases/account/load-account-by-token/load-account-by-token.ts
@@ -7,7 +7,7 @@ export class LoadAccountByTokenUseCase implements LoadAccountByToken {
   constructor (
     private readonly decrypter: Decrypter,
     private readonly loadAccountByTokenRepository: LoadAccountByTokenRepository
-  ) {}
+  ) { }
 
   load = async (accessToken: string, role?: string): Promise<AccountModel | null> => {
     const token = await this.decrypter.decrypt(accessToken);

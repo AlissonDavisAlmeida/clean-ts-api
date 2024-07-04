89200279a2b546b2c839d225c7ce827f94ede5da
alisson <alissoneatena1@gmail.com>
2024-07-04 11:44:37 -0300

    feat: ensure AccountMongoRepository returns an account on loadByToken with role

--- src/infra/db/mongodb/account/account-mongo.repository.spec.ts
+++ src/infra/db/mongodb/account/account-mongo.repository.spec.ts
@@ -99,4 +99,21 @@ describe('Account MongoDBRepository', () => {
     expect(account).toBeTruthy();
     expect(account?.id).toBeTruthy();
   });
+
+  test('should return an account on loadByToken with role', async () => {
+    const sut = makeSut();
+
+    await accountCollection?.insertOne({
+      name: 'any_name',
+      email: 'any_mail@.com',
+      password: 'any_password',
+      accessToken: 'any_token',
+      role: 'any_role'
+    });
+
+    const account = await sut.loadByToken('any_token', 'any_role');
+
+    expect(account).toBeTruthy();
+    expect(account?.id).toBeTruthy();
+  });
 });

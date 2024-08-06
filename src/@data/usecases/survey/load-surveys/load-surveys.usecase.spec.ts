c9d01905608f86524058e49e7492815bef236fa4
alisson <alissoneatena1@gmail.com>
2024-08-06 17:06:39 -0300

    feat: ensure LoadSurveysUseCase throws id LoadSurveysRepository throw an error

--- src/@data/usecases/survey/load-surveys/load-surveys.usecase.spec.ts
+++ src/@data/usecases/survey/load-surveys/load-surveys.usecase.spec.ts
@@ -1,6 +1,7 @@
 import { type LoadSurveysRepository } from '@/@data/protocols/db/survey/load-surveys.repository';
 import { LoadSurveysUseCase } from './load-surveys.usecase';
 import { type SurveyModel } from '@/@domain/models/SurveyModel';
+import MockDate from 'mockdate';
 
 type SutTypes = {
   sut: LoadSurveysUseCase
@@ -54,6 +55,13 @@ const makeSut = (): SutTypes => {
 };
 
 describe('LoadSurveysUseCase', () => {
+  beforeAll(() => {
+    MockDate.set(new Date());
+  });
+
+  afterAll(() => {
+    MockDate.reset();
+  });
   test('Should call LoadSurveysRepository', async () => {
     const { sut, loadSurveysRepositoryStub } = makeSut();
     const loadAllSpy = jest.spyOn(loadSurveysRepositoryStub, 'loadAll');

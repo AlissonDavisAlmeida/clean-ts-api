dfaab5357879284e04e294075df4ccb08c652459
alisson <alissoneatena1@gmail.com>
2024-08-10 19:35:10 -0300

    refactor: create own authMiddleware for router get surveys

--- src/main/routes/survey/survey-routes.ts
+++ src/main/routes/survey/survey-routes.ts
@@ -6,6 +6,7 @@ import { makeAuthMiddleware } from '@/main/factories/middlewares/auth-middleware
 
 export default (router: Router): void => {
   const adminAuth = adaptMiddleware(makeAuthMiddleware('admin'));
+  const auth = adaptMiddleware(makeAuthMiddleware(''));
   router.post('/surveys', adminAuth, adaptRoute(makeAddSurveyController()));
-  router.get('/surveys', adminAuth, adaptRoute(makeLoadSurveysController()));
+  router.get('/surveys', auth, adaptRoute(makeLoadSurveysController()));
 };
